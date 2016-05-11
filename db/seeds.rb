58c8a1b093d8cd4ec1ff8c6ad585d5f8ece7e134
Edouard de Nettancourt <edouard_de_nettancourt@hotmail.com>
2016-05-11 16:31:26 +0200

    Yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::Company.duns_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    )
+  restaurant.save
+end
+
+
+5.times do
+  review = Review.new(
+    rating: [0,1,2,3,4,5].sample,
+    content: Faker::Lorem.sentence,
+    restaurant_id: [1,2,3,4,5].sample
+    )
+  review.save
+end
